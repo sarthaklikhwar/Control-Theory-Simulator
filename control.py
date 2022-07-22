4ae0d3db11d50533676ba49d2739ffd495fe578b
sarthak <sarthaklikhwar29@gmail.com>
2022-07-22 13:10:25 +0530

    werw

--- control.py
+++ control.py
@@ -15,9 +15,9 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     t1: The torques required for the motor 1 for the current state
     t2: The torques required for the motor 2 for the current state
     '''
-    a=math.cos(q1-q1_desired)
+    a=math.cos(q1_desired)
     b=math.cos(q1_desired+q2_desired)
-    c=math.sin(q2-q2_desired)
+    c=math.sin(q2_desired)
     d=math.sin(q2-q2_desired)
     e=math.cos(q1_desired+q2_desired)
     # edit the code given below
@@ -25,3 +25,7 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     t2 = ((w1*w1*d)+(9.8*e))
 
     return t1, t2
+##g=-(math.pi/2)
+##h=-(math.pi/2)
+##q=(math.pi/2)
+##print(controller(g,h,1,1,q,0))
