058907d6c760184a8152d015fac53c6a1aed405e
sarthak <sarthaklikhwar29@gmail.com>
2022-07-22 12:21:19 +0530

    qw

--- control.py
+++ control.py
@@ -17,7 +17,7 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     t2: The torques required for the motor 2 for the current state
     '''
     # edit the code given below
-    t1 = -(2*math.sin(q2_desired)*w1*w2)-(math.sin(q2_desired)*w2*w2)+(2*9.8*math.sin(q1_desired))+(9.8*math.sin(q1_desired+q2_desired))
-    t2 = 1+(math.sin(q2_desired)*w1*w1)+(9.8*math.sin(q1_desired+q2_desired))
+    t1 = 0.4
+    t2 = 0.3
 
     return t1, t2
