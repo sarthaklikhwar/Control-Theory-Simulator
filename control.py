b3a08328a367498b2273f6f4072380222ecf7855
sarthak <sarthaklikhwar29@gmail.com>
2022-07-22 12:52:57 +0530

    gfdfd

--- control.py
+++ control.py
@@ -17,7 +17,7 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     t2: The torques required for the motor 2 for the current state
     '''
     # edit the code given below
-    t1 = int((2*9.8*math.cos(q1_desired))+(9.8*math.cos(q1_desired+q2_desired))-(math.sin(q2_desired)(2*w1*w2+w2*w2)))
-    t2 = int((w1*w1*math.sin(q2_desired))+(9.8*math.cos(q1_desired+q2_desired)))
+    t1 = int((2*10*math.cos(q1_desired))+(10*math.cos(q1_desired+q2_desired))-(math.sin(q2_desired)(2*w1*w2+w2*w2)))
+    t2 = int((w1*w1*math.sin(q2_desired))+(10*math.cos(q1_desired+q2_desired)))
 
     return t1, t2
