9b753a3348f4cb8a15c1a082efcbd0aba09bba50
sarthak <sarthaklikhwar29@gmail.com>
2022-07-25 12:24:33 +0530

    sdfas

--- control.py
+++ control.py
@@ -17,8 +17,8 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     '''
   
     # edit the code given below
-    t1=2*(70*(q1_desired-q1)-(10*w1))+math.cos(q1_desired-q2_desired)*(70*(q2_desired-q2)-(10*w2))
-    t2=math.cos(q1_desired-q2_desired)*(70*(q1_desired-q1)-(10*w1))+(70*(q2_desired-q2))-10*w2
+    t1=2*(50*(q1_desired-q1)-(10*w1))+math.cos(q1_desired-q2_desired)*(50*(q2_desired-q2)-(10*w2))
+    t2=math.cos(q1_desired-q2_desired)*(50*(q1_desired-q1)-(10*w1))+(50*(q2_desired-q2))-10*w2
     return t1, t2
 g=-(math.pi/2)
 h=-(math.pi/2)
