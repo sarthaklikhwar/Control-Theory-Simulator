01b86ec6d534cfaf956aafe040b6bd6b667c65ae
sarthak <sarthaklikhwar29@gmail.com>
2022-07-25 12:11:26 +0530

    hjj

--- control.py
+++ control.py
@@ -17,8 +17,8 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     '''
   
     # edit the code given below
-    t1=2*(32*(q1_desired-q1)-(17*w1))+math.cos(q1_desired-q2_desired)*(32*(q2_desired-q2)-(12*w2))
-    t2=math.cos(q1_desired-q2_desired)*(32*(q1_desired-q1)-(17*w1))+(32*(q2_desired-q2))-12*w2
+    t1=2*(32*(q1_desired-q1)-(18*w1))+math.cos(q1_desired-q2_desired)*(32*(q2_desired-q2)-(15*w2))
+    t2=math.cos(q1_desired-q2_desired)*(32*(q1_desired-q1)-(18*w1))+(32*(q2_desired-q2))-15*w2
     return t1, t2
 g=-(math.pi/2)
 h=-(math.pi/2)
