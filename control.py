65d7476b800c466c6fad09dafbdea9e116af4438
sarthak <sarthaklikhwar29@gmail.com>
2022-07-26 23:36:33 +0530

    sdhfiadsh

--- control.py
+++ control.py
@@ -17,8 +17,8 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     '''
   
     # edit the code given below
-    a1=7000*(q1_desired-q1)-100*w1
-    a2=7000*(q2_desired-q2)-100*w2
+    a1=7000*(q1_desired-q1)-180*w1
+    a2=7000*(q2_desired-q2)-180*w2
     t1 = (3+2*math.cos(q2))*a1+(1+math.cos(q2)*a2+0.5*(2*math.sin(q2)*w1*w2+math.sin(q2)*w2*w2)+0.5*9.8*(2*math.sin(q1))+math.sin(q1+q2))
     t2 = (1+math.cos(q2))*a1+a2-0.5*math.sin(q2)*w1*w1+0.5*9.8*math.sin(q1+q2)
     return t1, t2
